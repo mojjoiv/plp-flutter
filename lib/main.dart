cd8a89d5b9c362201bfa9932b21765926df4e836
mojjoiv <daltonongeche@gmail.com>
2024-04-27 20:21:42 +0300

    task manager with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todolist/screens/tasks_screen.dart';// importing the task_screen.dart
+import 'package:provider/provider.dart';
+import 'package:todo/layout/home_layout.dart';
+import 'package:todo/screens/edit/edit_screen.dart';
+import 'package:todo/screens/login/login_screen.dart';
+import 'package:todo/shared/styles/my_theme_data.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+import 'providers/provider.dart';
+
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+      ChangeNotifierProvider(create: (context) => MyProvider(), child: todo()));
 }
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+class todo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<MyProvider>(context);
     return MaterialApp(
-      debugShowCheckedModeBanner: false,  // Hide debug banner
-      title: 'Flutter Demo',  // Set app title
-      theme: ThemeData(  // Configure app theme
-        colorScheme: ColorScheme.fromSwatch().copyWith(primary: const Color(0xFF883007)),  // Set primary color
-        useMaterial3: true,  // Enable Material 3 design elements
-      ),
-      home: TasksScreen(),  // Set home screen to be  TasksScreen
+      debugShowCheckedModeBanner: false,
+      themeMode: provider.modeApp,
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darkTheme,
+      routes: {
+        HomeLayout.routeName: (context) => HomeLayout(),
+        EditScreen.routeName: (context) => EditScreen(),
+        LoginScreen.routeName: (context) => LoginScreen(),
+      },
+      initialRoute: provider.firebaseUser != null
+          ? HomeLayout.routeName
+          : LoginScreen.routeName,
     );
   }
 }
